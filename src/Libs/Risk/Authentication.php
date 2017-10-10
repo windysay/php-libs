b06c96dd3cbfe7fcc2c84ff89df4ef7ec55dc4e4
zou.cx <471500439@qq.com>
2017-10-10 09:38:14 +0800

    [*]域名配置

--- src/Libs/Risk/Authentication.php
+++ src/Libs/Risk/Authentication.php
@@ -4,10 +4,13 @@ namespace JMD\Libs\Risk;
 class Authentication
 {
 
-    public static function getAccessToken($appKey, $secretKey)
+    public static function getAccessToken($appKey, $secretKey, $domain = null)
     {
         //TODO 第一版本，此处有些违反单一职责原则
         $request = new BaseRequest();
+        if ($domain) {
+            $request->setDomain($domain);
+        }
         $request->setUrl('account/login');
         $request->setData([
             'app_key' => $appKey,
