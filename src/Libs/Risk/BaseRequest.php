d86c65a7663f985eba45dddcd5872189f6d36253
zou.cx <471500439@qq.com>
2017-10-12 14:54:39 +0800

    [*]认证方式修改

--- src/Libs/Risk/BaseRequest.php
+++ src/Libs/Risk/BaseRequest.php
@@ -93,7 +93,6 @@ class BaseRequest implements Request
             }
             $this->data['sign'] =  md5(json_encode($this->data));
             $result= HttpHelper::post($url, $this->data);
-            echo $result;
             return $result;
         } else {
             return HttpHelper::get($url);
