9dff0d794d038ddb326475562b325a5f21e5f2f6
name <email@mail.com>
2018-10-24 10:47:49 +0800

    [*]加返回方法，疑似弃用流程注释

--- src/Utils/SsoHelper.php
+++ src/Utils/SsoHelper.php
@@ -4,9 +4,13 @@ namespace JMD\Utils;
 
 use JMD\App\Utils;
 use JMD\Cache\Cookie\Sso\TicketCookie;
+use JMD\Libs\Services\BaseRequest;
 
 class SsoHelper
 {
+    const LOGIN_STATUS_SUCCESS = 200;//已登录，成功登录
+    const LOGIN_STATUS_FAIL = 1001;//未登录，已退出，过期
+
     /**
      * @param $ticket
      * @return mixed
@@ -24,5 +28,43 @@ class SsoHelper
         //否则读cookie的ticket
         $ticket = TicketCookie::get();
         return $ticket;
+    }
+
+    /**
+     * 统一登录失败返回
+     *
+     * @param string $msg
+     * @return array
+     */
+    public static function resFail($msg = '')
+    {
+        return [
+            'code' => self::LOGIN_STATUS_FAIL,
+            'sso_login_url' => self::getSsoLoginUrl(),
+            'sso_logout_url' => self::getSsoLogoutUrl(),
+            'msg' => $msg,
+        ];
+    }
+
+    /**
+     * 获取登录地址
+     *
+     * @return string
+     */
+    public static function getSsoLoginUrl()
+    {
+        $config = Utils::getParam(BaseRequest::CONFIG_NAME);
+        return $config['sso_endpoint'] . 'sso/login';
+    }
+
+    /**
+     * 获取退出地址
+     *
+     * @return string
+     */
+    public static function getSsoLogoutUrl()
+    {
+        $config = Utils::getParam(BaseRequest::CONFIG_NAME);
+        return $config['sso_endpoint'] . 'sso/logout';
     }
 }
