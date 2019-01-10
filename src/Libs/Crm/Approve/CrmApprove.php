0ca67c8bab2f321ab5a56169beeba169ac18c757
name <email@mail.com>
2019-01-10 18:23:34 +0800

    [*]取消

--- src/Libs/Crm/Approve/CrmApprove.php
+++ src/Libs/Crm/Approve/CrmApprove.php
@@ -12,7 +12,17 @@ use JMD\Libs\Crm\BaseRequest;
 
 class CrmApprove
 {
-    public static function cancel($orderId)
+    /**
+     * 用户取消/人工取消
+     */
+    const NORMAL_CANCEL = 1;
+
+    /**
+     * 定时任务取消
+     */
+    const CRONTAB_CANCEL = 2;
+
+    public static function cancel($orderId, $type = self::NORMAL_CANCEL)
     {
         $request = new BaseRequest();
         $url = 'api/main/approve-cancel';
@@ -20,6 +30,7 @@ class CrmApprove
 
         $postData = [
             'orderId' => $orderId,
+            'type' => $type
         ];
         $request->setData($postData);
         return $request->execute();
